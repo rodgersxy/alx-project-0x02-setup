7ed75cb6dd9b35a8fb0d808674688043592320b9
rodgersxy <rodgersny99@gmail.com>
2025-01-08 18:42:39 +0300

    add: 2. Basic Routing in Next.js

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Header: React.FC = () => {
     return (
-        <header>
-            <h1>Welcome to ALX Project</h1>
+        <header style={{ display: 'flex', gap: '10px', padding: '10px' }}>
+            <h1>ALX Project</h1>
+            <nav>
+                <Link href="/home">Home</Link>
+                <Link href="/about">About</Link>
+            </nav>
         </header>
     );
 };
